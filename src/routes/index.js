747a16b7f4bd08596f26b3dbe8d4034b1f84c478
ShrutiKumariMahata <shrutikumarimahata@gmail.com>
2026-06-02 01:36:04 +0530

    conn

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,16 @@
-// TODO: Add implementation
+import { Router } from 'express';
+import notificationRoutes from '../modules/share/notification/notification.router.js';
+
+const router = Router();
+
+// Mount notification module under /api/notifications
+router.use('/notifications', notificationRoutes);
+
+// If you have other module routes, mount them here:
+// router.use('/auth', authRoutes);
+// router.use('/schools', schoolRoutes);
+// router.use('/students', studentRoutes);
+// router.use('/communication', communicationRoutes);
+// etc.
+
+export default router;
